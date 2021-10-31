2a913fd0a57cec415ada96be8f92e335ab7d70ab
codelixir <pahulpreet.singh@research.iiit.ac.in>
2021-10-31 22:57:35 +0530

    non preemptive

--- kernel/trap.c
+++ kernel/trap.c
@@ -150,9 +150,11 @@ void kerneltrap()
     panic("kerneltrap");
   }
 
+#ifdef RR
   // give up the CPU if this is a timer interrupt.
   if (which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING)
     yield();
+#endif
 
   // the yield() may have caused some traps to occur,
   // so restore trap registers for use by kernelvec.S's sepc instruction.
